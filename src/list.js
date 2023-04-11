72f67a0369ec94dbbe0d06d1ea78ab5596de2896
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-04-11 21:10:39 +0300

    Remove extra code

--- src/list.js
+++ src/list.js
@@ -1,17 +1,3 @@
-// const tallys = [
-//   { name: 'Brian', score: 100 },
-//   { name: 'Ted', score: 20 },
-//   { name: 'Bob', score: 50 },
-//   { name: 'Jane', score: 78 },
-//   { name: 'Mary', score: 125 },
-//   { name: 'John', score: 77 },
-//   { name: 'Alice', score: 42 },
-// ];
-
-
-// const box = document.createElement('div');
-// 
-
 const updateList = async () => {
   try{
     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XnkekTLELw5pX9NuHdI1/scores')
@@ -30,18 +16,5 @@ const updateList = async () => {
     console.error(error);
   }
   };
-
-  // list.forEach((item) => {
-  //   board.appendChild(item);
-  // });
-
-  //const list = tallys.map((tally) => {
-    //   
-    //   description.innerHTML = `
-    //       <p> ${tally.name}: ${tally.score} </p>
-    //       `;
-  
-    //   box.appendChild(description);
-    //   return box;
 
 export default updateList;
