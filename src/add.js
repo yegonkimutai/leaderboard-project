ce02fae4f98e26a2481a4c7e6c8965a9b342da31
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-04-12 08:06:43 +0300

    Update content

--- src/add.js
+++ src/add.js
@@ -1,28 +1,25 @@
 const addScore = () => {
-    const scoreList = document.querySelector('.form-entry');
+  const scoreList = document.querySelector('.form-entry');
 
-    scoreList.addEventListener('submit', async(e) => {
-        e.preventDefault();
+  scoreList.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-        const userName = document.querySelector('.name').value;
-        const userScore = document.querySelector('.score').value;
-        try {
-            await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XnkekTLELw5pX9NuHdI1/scores',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    user: userName,
-                    score: userScore,
-                })
-            });
-            const refresh = document.querySelector('.refresh');
-            refresh.click();
-        } catch (error) {
-        console.error('Error adding new score:', error);
-        }
-    })
-}
+    const userName = document.querySelector('.name').value;
+    const userScore = document.querySelector('.score').value;
+
+    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yAtQ05PVzJj2vldtLJCO/scores', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: userName,
+        score: userScore,
+      }),
+    });
+    const refresh = document.querySelector('.refresh');
+    refresh.click();
+  });
+};
 
 export default addScore;
--- src/list.js
+++ src/list.js
@@ -1,20 +1,16 @@
 const updateList = async () => {
-  try{
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/XnkekTLELw5pX9NuHdI1/scores')
-    const data = await response.json();
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yAtQ05PVzJj2vldtLJCO/scores');
+  const data = await response.json();
 
-    const board = document.querySelector('.scores');
-    board.innerHTML = '';
+  const board = document.querySelector('.scores');
+  board.innerHTML = '';
 
-    data.result.forEach((score) => {
-      const description = document.createElement('div');
-      description.classList.add('score-board');
-      description.textContent = `${score.user}: ${score.score}`;
-      board.appendChild(description)
-    })
-  } catch (error) {
-    console.error(error);
-  }
-  };
+  data.result.forEach((score) => {
+    const description = document.createElement('div');
+    description.classList.add('score-board');
+    description.textContent = `${score.user}: ${score.score}`;
+    board.appendChild(description);
+  });
+};
 
 export default updateList;
